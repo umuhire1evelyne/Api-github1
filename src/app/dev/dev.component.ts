f283de6e528c8133a8ecc32d59d8cf651cd41db0
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-09-06 20:11:56 +0200

    dev comp.ts was added

---
+++ src/app/dev/dev.component.ts
@@ -0,0 +1,42 @@
+// // import { Component, OnInit } from '@angular/core';
+
+// // @Component({
+// //   selector: 'app-dev',
+// //   templateUrl: './dev.component.html',
+// //   styleUrls: ['./dev.component.css']
+// // })
+// // export class DevComponent implements OnInit {
+
+// //   constructor() { }
+
+// //   ngOnInit() {
+// //   }
+
+// // }
+
+import { Component, OnInit } from '@angular/core';
+import { Dev } from '../dev';
+import { DevRequestService } from '../dev-request.service';
+// import {SearchFormComponent} from '../search-form/search-form';
+
+
+@Component({
+  selector: 'app-dev',
+  templateUrl: './dev.component.html',
+  styleUrls: ['./dev.component.css'],
+  providers: [ DevComponent ]
+})
+export class DevComponent implements OnInit {
+
+  dev: Dev;
+
+  constructor(private devService: DevRequestService) { }
+
+  ngOnInit() {
+    // this.devService.createUrl();
+    this.devService.devRequest();
+    this.dev = this.devService.dev;
+  }
+
+
+}
