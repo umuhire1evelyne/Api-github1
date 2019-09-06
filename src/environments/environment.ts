5f8661ba252413d58b120207cd235dea4ac98d1c
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-09-06 20:07:36 +0200

    environment.ts was also added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,34 @@
+// // This file can be replaced during build by using the `fileReplacements` array.
+// // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// // The list of file replacements can be found in `angular.json`.
+
+// export const environment = {
+//   production: false
+// };
+
+// /*
+//  * For easier debugging in development mode, you can import the following file
+//  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+//  *
+//  * This import should be commented out in production mode because it will have a negative impact
+//  * on performance if an error is thrown.
+//  */
+// // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
 // This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://api.github.com/users/umuhire1evelyne?accessntoken=10f0cb49fefb6c3a086b705882ac461a99d93464',
+  repoApi: 'https://api.github.com/users/umuhire1evelyne/repos'
 };
 
 /*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
