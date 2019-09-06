27117d11d9ca4f724f832670314e028355ec6709
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-09-06 20:04:56 +0200

    app-routing.ts was added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { RepoComponent } from './repo/repo.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'repo', component : RepoComponent},
+  { path: 'search', component : SearchFormComponent},
+  { path: '', redirectTo:"/search", pathMatch:"full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  
+ }
