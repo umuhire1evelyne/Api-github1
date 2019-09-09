4032c29970b9bfe4af0a8877d8c413c359c173c4
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-09-10 00:01:57 +0200

    repo com.ts was added

--- src/app/repo/repo.component.ts
+++ src/app/repo/repo.component.ts
@@ -1,36 +1,36 @@
-// import { Component, OnInit } from '@angular/core';
+// // import { Component, OnInit } from '@angular/core';
 
+// // @Component({
+// //   selector: 'app-repo',
+// //   templateUrl: './repo.component.html',
+// //   styleUrls: ['./repo.component.css']
+// // })
+// // export class RepoComponent implements OnInit {
+
+// //   constructor() { }
+
+// //   ngOnInit() {
+// //   }
+
+// // }
+
+// import { Component, OnInit } from '@angular/core';
+// import { Dev } from '../dev';
+// import { DevComponent } from '../dev/dev.component';
+// import { DevRequestService } from '../dev-request.service';
 // @Component({
 //   selector: 'app-repo',
 //   templateUrl: './repo.component.html',
-//   styleUrls: ['./repo.component.css']
+//   styleUrls: ['./repo.component.css'],
+//   providers: [ DevComponent ]
 // })
 // export class RepoComponent implements OnInit {
-
-//   constructor() { }
+//   dev: Dev;
+//   constructor(private devService: DevRequestService) { }
 
 //   ngOnInit() {
+//     this.devService.userRepoRequest();
+//     this.dev = this.devService.dev;
 //   }
 
 // }
-
-import { Component, OnInit } from '@angular/core';
-import { Dev } from '../dev';
-import { DevComponent } from '../dev/dev.component';
-import { DevRequestService } from '../dev-request.service';
-@Component({
-  selector: 'app-repo',
-  templateUrl: './repo.component.html',
-  styleUrls: ['./repo.component.css'],
-  providers: [ DevComponent ]
-})
-export class RepoComponent implements OnInit {
-  dev: Dev;
-  constructor(private devService: DevRequestService) { }
-
-  ngOnInit() {
-    this.devService.userRepoRequest();
-    this.dev = this.devService.dev;
-  }
-
-}
