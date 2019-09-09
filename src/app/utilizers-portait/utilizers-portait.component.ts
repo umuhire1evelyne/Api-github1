c96533f9ecd79d721b5b3988202dcb3abc0405f5
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-09-10 00:55:52 +0200

    utilizers.ts was added

---
+++ src/app/utilizers-portait/utilizers-portait.component.ts
@@ -0,0 +1,15 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-utilizers-portait',
+//   templateUrl: './utilizers-portait.component.html',
+//   styleUrls: ['./utilizers-portait.component.css']
+// })
+// export class UtilizersPortaitComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
