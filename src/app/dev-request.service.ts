5312b36ce3687610c453c0e776e2d3686996abcc
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-09-10 00:12:27 +0200

    dev-request.ts was added

--- src/app/dev-request.service.ts
+++ src/app/dev-request.service.ts
@@ -8,62 +8,62 @@
 //   constructor() { }
 // }
 
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../environments/environment';
-import { Dev } from './dev';
-import {SearchFormComponent} from './search-form/search-form.component';
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { environment } from '../environments/environment';
+// import { Dev } from './dev';
+// import {SearchFormComponent} from './search-form/search-form.component';
 
 
-@Injectable()
-export class DevRequestService {
+// @Injectable()
+// export class DevRequestService {
 
-  dev: Dev;
+//   dev: Dev;
 
-  constructor(private http: HttpClient) {
-  this.dev = new Dev('', '', ''); }
+//   constructor(private http: HttpClient) {
+//   this.dev = new Dev('', '', '','',''); }
 
 
-  devRequest() {
-    interface ApiResponse {
-      login: string;
-      avatar_url: string;
-      repo_url: any;
-    }
+//   devRequest() {
+//     interface ApiResponse {
+//       login: string;
+//       avatar_url: string;
+//       repo_url: any;
+//     }
 
-  const promise = new Promise((resolve, reject) => {
-    this.http.get<ApiResponse>( environment.apiUrl).toPromise().then(response => {
-      this.dev.name = response.login;
-      this.dev.image = response.avatar_url;
+//   const promise = new Promise((resolve, reject) => {
+//     this.http.get<ApiResponse>( environment.apiUrl).toPromise().then(response => {
+//       this.dev.name = response.login;
+//       this.dev.image = response.avatar_url;
 
-      resolve();
-    },
-    error => {
-      this.dev.name = 'Experiencing some tech difficulties';
+//       resolve();
+//     },
+//     error => {
+//       this.dev.name = 'Experiencing some tech difficulties';
 
-      reject(error);
-    });
-    });
-    return promise;
-  }
+//       reject(error);
+//     });
+//     });
+//     return promise;
+//   }
 
-  userRepoRequest() {
-    interface RepoResponse {
-      repo_url: any;
-    }
+//   userRepoRequest() {
+//     interface RepoResponse {
+//       repo_url: any;
+//     }
 
-  const promise1 = new Promise((resolve, reject) => {
-    this.http.get<RepoResponse>(environment.repoApi).toPromise().then(response => {
-      this.dev.repo = response;
-      resolve();
-    },
-    error => {
-      this.dev.repo = 'Experiencing some tech difficulties';
+//   const promise1 = new Promise((resolve, reject) => {
+//     this.http.get<RepoResponse>(environment.repoApi).toPromise().then(response => {
+//       this.dev.repo = response;
+//       resolve();
+//     },
+//     error => {
+//       this.dev.repo = 'Experiencing some tech difficulties';
 
-      reject(error);
-    });
-    });
-    return promise1;
-  }
+//       reject(error);
+//     });
+//     });
+//     return promise1;
+//   }
 
-}
+// }
