3eb59ed3aba2daaeb35cb3bc209f2a7e82170317
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-09-10 00:50:27 +0200

    view.ts was added

--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -13,3 +13,19 @@
 //   }
 
 // }
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view',
+  templateUrl: './view.component.html',
+  styleUrls: ['./view.component.css']
+})
+export class ViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
