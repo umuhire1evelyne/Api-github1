0126678b0b83e9fb5c968fd71b79d05cd8adcfe1
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-09-10 00:02:45 +0200

    repo com.ts was added

--- src/app/repo/repo.component.ts
+++ src/app/repo/repo.component.ts
@@ -34,3 +34,40 @@
 //   }
 
 // }
+
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-repo',
+//   templateUrl: './repo.component.html',
+//   styleUrls: ['./repo.component.css']
+// })
+// export class RepoComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
+
+import { Component, OnInit } from '@angular/core';
+import { Dev } from '../dev';
+import { DevComponent } from '../dev/dev.component';
+import { DevRequestService } from '../dev-request.service';
+@Component({
+  selector: 'app-repo',
+  templateUrl: './repo.component.html',
+  styleUrls: ['./repo.component.css'],
+  providers: [ DevComponent ]
+})
+export class RepoComponent implements OnInit {
+  dev: Dev;
+  constructor(private devService: DevRequestService) { }
+
+  ngOnInit() {
+    this.devService.userRepoRequest();
+    this.dev = this.devService.dev;
+  }
+
+}
