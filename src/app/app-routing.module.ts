4c8d54a414e12b2307888e015f3652dfee655366
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-09-09 23:22:33 +0200

    app-routing.ts was added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,40 @@
-import { NgModule, Component } from '@angular/core';
+// import { NgModule, Component } from '@angular/core';
+// import { Routes, RouterModule } from '@angular/router';
+// import { SearchFormComponent } from './search-form/search-form.component';
+// import { RepoComponent } from './repo/repo.component';
+
+
+// const routes: Routes = [
+//   { path: 'repo', component : RepoComponent},
+//   { path: 'search', component : SearchFormComponent},
+//   { path: '', redirectTo:"/search", pathMatch:"full"},
+// ];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule {
+  
+//  }
+
+import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SearchFormComponent } from './search-form/search-form.component';
-import { RepoComponent } from './repo/repo.component';
+import { UtilizersPortraitComponent } from 'src/app/utilizers-portrait/utilizers-portrait.component';
+import { TheUserComponent } from 'src/app/the-user/the-user.component';
+import { ViewComponent } from 'src/app/view/view.component';
+import { CommonModule } from '@angular/common';
 
 
 const routes: Routes = [
-  { path: 'repo', component : RepoComponent},
-  { path: 'search', component : SearchFormComponent},
-  { path: '', redirectTo:"/search", pathMatch:"full"},
+  { path: '', component: ViewComponent},
+  { path: 'user/repositories?', component: TheUserComponent},
+  { path: 'userprofile?', component: UtilizersPortraitComponent},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+CommonModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-  
- }
+export class AppRoutingModule { }
