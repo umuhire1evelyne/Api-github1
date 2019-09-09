888e7ed348e55cec92f580e9774b80b13c06e991
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-09-10 00:10:13 +0200

    dev.ts was added

--- src/app/dev.ts
+++ src/app/dev.ts
@@ -1,3 +1,7 @@
 // export class Dev {
 //   constructor(public name: string, public image: string, public repo: any,public avatar_url:string,public login:string) {}
 // }
+
+export class Dev {
+  constructor(public name: string, public image: string, public repo: any,public avatar_url:string,public login:string) {}
+}
