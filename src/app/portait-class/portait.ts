c343c597123fe0419265688cb33bd1c8de43a929
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-09-10 01:12:19 +0200

    portait-class.ts was added

---
+++ src/app/portait-class/portait.ts
@@ -0,0 +1,9 @@
+// export class Portait {
+// }
+
+export class Portrait {
+    constructor(public login: string, public avatar_url: string, public followers_url: string,
+       public following_url: string,
+        public repos_url: string, public name: string, public location: string, public email: string,
+         public repo_url: string, public createdAt: Date) {}
+  }
