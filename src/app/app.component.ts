0e038d5f87b3b410d2a9f44d76f75e7e00dc1b37
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-09-09 23:20:04 +0200

    app.ts was added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,15 @@
 
 // import { Component } from '@angular/core';
 
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   title = 'Git-Search';
-// }
 
 
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'GitHub-Search';
+}
