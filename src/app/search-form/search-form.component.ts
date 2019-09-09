269d45345e98dc6463fa7cce720d2ee4ff0e4438
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-09-09 23:55:16 +0200

    search form.ts was added

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -1,3 +1,65 @@
+// // import { Component, OnInit } from '@angular/core';
+
+// // @Component({
+// //   selector: 'app-search-form',
+// //   templateUrl: './search-form.component.html',
+// //   styleUrls: ['./search-form.component.css']
+// // })
+// // export class SearchFormComponent implements OnInit {
+
+// //   constructor() { }
+
+// //   ngOnInit() {
+// //   }
+
+// // }
+
+
+// import { Component, OnInit } from '@angular/core';
+// import {Dev} from '../dev';
+// import {HttpClient} from '@angular/common/http';
+// import { resolve } from 'q';
+// // import {DevRequestService} from '../dev-request.service';
+// // import {Repo} from '../repo';
+
+
+// @Component({
+//   selector: 'app-search-form',
+//   templateUrl: './search-form.component.html',
+//   styleUrls: ['./search-form.component.css'],
+
+// })
+// export class SearchFormComponent implements OnInit {
+//   // const repo = new Repo();
+//    dev = new Dev('', '', '', '','');
+//   constructor(private http: HttpClient) { }
+
+//   searcher(search: string) {
+//     let x = 'https://api.github.com/users/' + search;
+//     this.http.get(x).subscribe(answer => {
+//       console.log(answer);
+//       // this.dev.name = answer.login;
+//       // this.dev.image = answer.avatar_url;
+//       resolve(search);
+//     });
+//     let y = 'https://api.github.com/users/' + search + '/repos' ;
+//     this.http.get(y).subscribe(data => {
+//        this.dev.repo = data;
+//     });
+//   }
+
+
+
+
+//   ngOnInit() {
+//   }
+
+// }
+
+
+
+
+  
 // import { Component, OnInit } from '@angular/core';
 
 // @Component({
@@ -18,8 +80,9 @@
 import { Component, OnInit } from '@angular/core';
 import {Dev} from '../dev';
 import {HttpClient} from '@angular/common/http';
-import {DevRequestService} from '../dev-request.service';
-import {Repo} from '../repo';
+import { resolve } from 'q';
+// import {DevRequestService} from '../dev-request.service';
+// import {Repo} from '../repo';
 
 
 @Component({
@@ -30,15 +93,16 @@ import {Repo} from '../repo';
 })
 export class SearchFormComponent implements OnInit {
   // const repo = new Repo();
-  const dev = new Dev('', '', '');
+   dev = new Dev('', '', '', '','');
   constructor(private http: HttpClient) { }
 
   searcher(search: string) {
     let x = 'https://api.github.com/users/' + search;
     this.http.get(x).subscribe(answer => {
-      // console.log(answer);
-      this.dev.name = answer.login;
-      this.dev.image = answer.avatar_url;
+      console.log(answer);
+      // this.dev.name = answer.login;
+      // this.dev.image = answer.avatar_url;
+      resolve(search);
     });
     let y = 'https://api.github.com/users/' + search + '/repos' ;
     this.http.get(y).subscribe(data => {
