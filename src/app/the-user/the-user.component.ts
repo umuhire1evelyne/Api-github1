26a8236727362acca95556af3b2b3e2ecb745883
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-09-10 00:40:22 +0200

    the-user.ts was added

---
+++ src/app/the-user/the-user.component.ts
@@ -0,0 +1,15 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-the-user',
+//   templateUrl: './the-user.component.html',
+//   styleUrls: ['./the-user.component.css']
+// })
+// export class TheUserComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
