afa6a0dc38a2f83c8054d4837095e17430556708
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-09-10 00:06:48 +0200

    dev com.ts was added

--- src/app/dev/dev.component.ts
+++ src/app/dev/dev.component.ts
@@ -14,29 +14,30 @@
 
 // // }
 
-import { Component, OnInit } from '@angular/core';
-import { Dev } from '../dev';
-import { DevRequestService } from '../dev-request.service';
-// import {SearchFormComponent} from '../search-form/search-form';
+// import { Component, OnInit } from '@angular/core';
+// import { Dev } from '../dev';
+// import { DevRequestService } from '../dev-request.service';
+// // import {SearchFormComponent} from '../search-form/search-form';
 
 
-@Component({
-  selector: 'app-dev',
-  templateUrl: './dev.component.html',
-  styleUrls: ['./dev.component.css'],
-  providers: [ DevComponent ]
-})
-export class DevComponent implements OnInit {
+// @Component({
+//   selector: 'app-dev',
+//   templateUrl: './dev.component.html',
+//   styleUrls: ['./dev.component.css'],
+//   providers: [ DevComponent ]
+// })
+// export class DevComponent implements OnInit {
 
-  dev: Dev;
+//   dev: Dev;
 
-  constructor(private devService: DevRequestService) { }
+//   constructor(private devService: DevRequestService) { }
+  
 
-  ngOnInit() {
-    // this.devService.createUrl();
-    this.devService.devRequest();
-    this.dev = this.devService.dev;
-  }
+//   ngOnInit() {
+//     // this.devService.createUrl();
+//     this.devService.devRequest();
+//     this.dev = this.devService.dev;
+//   }
 
 
-}
+// }
